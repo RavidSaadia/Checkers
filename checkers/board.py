1304df5eb21da9490eba95bff97b3d87ccc19269
RavidSaadia <ravids1233@gmail.com>
2021-04-20 15:43:27 +0300

    first commit

---
+++ checkers/board.py
@@ -0,0 +1,21 @@
+import pygame
+
+from checkers.constant import *
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.res_left = self.white_left = 12
+        self.res_kings = self.white_kings = 0
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE,
+                                            SQUARE_SIZE, SQUARE_SIZE))
+
+    def create_board(self):
+        pass
---
+++ checkers/piece.py
@@ -0,0 +1,22 @@
+from checkers.constant import *
+
+
+class Piece:
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.king = False
+
+        if self.color == RED:
+            self.direction = -1
+        else:
+            self.direction = 1
+
+        self.x = 0
+        self.y = 0
+        self.calculate_pos()
+
+    def calculate_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
